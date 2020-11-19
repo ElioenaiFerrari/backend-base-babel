4fca18f9e38b3e5b101a35f03639992264da22cd
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-19 20:03:15 -0300

    base

--- build/main.js
+++ build/main.js
@@ -2,7 +2,7 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _app = _interopRequireDefault(require("../config/app"));
+var _app = _interopRequireDefault(require("./config/app"));
 
 _app["default"].listen(process.env.PORT);
 //# sourceMappingURL=main.js.map
