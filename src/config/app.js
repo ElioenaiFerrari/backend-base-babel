dd09d5b6784917ca24716807c214557c6988eabd
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-21 19:15:41 -0300

    base

--- src/config/app.js
+++ src/config/app.js
@@ -2,7 +2,7 @@ import '@/database';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import injections from '@/injections';
+import Resources from '@/resources';
 
 const App = express();
 
@@ -11,6 +11,6 @@ App.use(express.json());
 App.use(express.urlencoded({ extended: false }));
 App.use(morgan('dev'));
 
-injections.routerInjections(App);
+Resources.routerInjections(App);
 
 export default App;
--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import Sequelize from 'sequelize';
 import dbConfig from '@/config/db';
-import Injections from '@/injections';
+import Resources from '@/resources';
 
 const { NODE_ENV } = process.env;
 
 const conn = new Sequelize(dbConfig[NODE_ENV]);
 
-Injections.modelInjections(conn);
+Resources.modelInjections(conn);
 
 export default conn;
