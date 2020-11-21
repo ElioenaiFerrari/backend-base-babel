ab67005c27f57a2f0db25657a4fe708723167c0c
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-21 20:33:03 -0300

    base

--- src/database/migrations/20201121230428-create_techs.js
+++ src/database/migrations/20201121230428-create_techs.js
@@ -4,6 +4,10 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('techs', {
       id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+      dev_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'devs', key: 'id' },
+      },
       name: { type: Sequelize.STRING, allowNull: false },
       created_at: { type: Sequelize.DATE, allowNull: false },
       updated_at: { type: Sequelize.DATE, allowNull: false },
