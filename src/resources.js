c8cd15e13d754dbacdad791c83cc7753a6efb661
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-21 19:17:32 -0300

    base

--- src/resources.js
+++ src/resources.js
@@ -1,9 +1,11 @@
 import requireDir from 'require-dir';
-import Injector from './injector';
+import Injector from '@/injector';
 const Models = requireDir('models');
 const Routers = requireDir('router');
 
 const modelInjections = Injector.injectModelsInConnection(Models);
 const routerInjections = Injector.injectDepsInApp(Routers);
 
-export default { modelInjections, routerInjections };
+const Resources = { modelInjections, routerInjections };
+
+export default Resources;
