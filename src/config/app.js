98e7db2e983e473735ee0a449773d94b3a9b8713
elioenaiferrari <elioenaiferrari@gmail.com>
2020-11-22 09:18:47 -0300

    base

--- src/config/app.js
+++ src/config/app.js
@@ -1,15 +1,8 @@
 import '@/database';
 import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
 import Resources from '@/resources';
 
 const App = express();
-
-App.use(cors({ origin: true }));
-App.use(express.json());
-App.use(express.urlencoded({ extended: false }));
-App.use(morgan('dev'));
 
 Resources.routerInjections(App);
 Resources.middlewaresInjections(App);
---
+++ src/middlewares/core.js
@@ -0,0 +1,10 @@
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+
+export default function (App) {
+  App.use(cors({ origin: true }));
+  App.use(express.json());
+  App.use(express.urlencoded({ extended: false }));
+  App.use(morgan('dev'));
+}
